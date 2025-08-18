6b6336003c3e0a1ab37c2c92787daa1b60714679
JayasriVijay <savithri87@gmail.com>
2025-08-18 07:22:50 -0400

    added other modules

---
+++ src/test/java/pageFactory/LinkedList_pf.java
@@ -0,0 +1,107 @@
+package pageFactory;
+import java.io.IOException;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+public class LinkedList_pf extends BasePage {
+ 
+	public LinkedList_pf() throws IOException {
+	    super();
+		
+    }
+	 @FindBy(xpath="//*[@href='introduction']")
+	 WebElement introPageLink;
+	 @FindBy(xpath="//*[contains(text(),'Practice Questions')]")
+	 WebElement practicePageLink;
+	 @FindBy(xpath="//*[@href='linked-list']")
+	 WebElement linkedListButton;
+	 @FindBy(xpath="//*[@href='/tryEditor']")
+	 WebElement tryHereButton;
+	 @FindBy(xpath="//*[@class='CodeMirror-scroll']")
+	 WebElement tryEditor;
+	 @FindBy(xpath="//*[contains(text(),'Run')]")
+	 WebElement runButton;
+	 @FindBy(xpath="//*[@id='output']")
+	 WebElement output;
+	 @FindBy(xpath="//*[@href='creating-linked-list']")
+	 WebElement createLinkedListLink;
+	 @FindBy(xpath="//*[@href='types-of-linked-list']")
+	 WebElement typesofLinkedListLink;
+	 @FindBy(xpath="//*[@href='implement-linked-list-in-python']")
+	 WebElement implementLinkedListLink;
+	 @FindBy(xpath="//*[@href='traversal']")
+	 WebElement traversalLink;
+	 @FindBy(xpath="//*[@href='insertion-in-linked-list']")
+	 WebElement insertionLink;
+	 @FindBy(xpath="//*[@href='deletion-in-linked-list']")
+	 WebElement deletionLink;
+	 
+	 public void getToLinkedList(){
+		 linkedListButton.click();     			
+		}
+	 
+	 public void clickOnIntroLink(){
+		 introPageLink.click();     	
+	}
+	 public void clickTryHere(){
+		 tryHereButton.click();     	
+	}
+	 
+	 public void clickOnPracticeQuestion() {
+		 practicePageLink.click();
+	 }
+	 
+	 public void clickOncreateLinkedListLink() {
+		 createLinkedListLink.click();
+	 }
+	 public void clickOntypesofLinkedList() {
+		 typesofLinkedListLink.click();
+	 }
+	 public void clickOnimplementLinkedList() {
+		 implementLinkedListLink.click();
+	 }
+	 public void clickOntraversal() {
+		 traversalLink.click();
+	 }
+	 public void clickOninsertion() {
+		 insertionLink.click();
+	 }
+	 public void clickOndeletionLink() {
+		 deletionLink.click();
+	 }
+	 public void navigateToIntroTryEditor() {
+	        clickOnIntroLink();
+	        clickTryHere();
+	    }
+	 public void navigateToCreateLinkedListTryEditor() {
+	       clickOncreateLinkedListLink();
+	       clickTryHere();
+	    }
+	 public void navigateToTypesofLinkedListTryEditor() {
+	       clickOntypesofLinkedList();
+	       clickTryHere();
+	    }
+	 public void navigateToImplementLinkedListTryEditor() {
+	       clickOnimplementLinkedList();
+	       clickTryHere();
+	    }
+	 public void navigateTonavigateToTraversalTryEditor() {
+	       clickOntraversal();
+	       clickTryHere();
+	    }
+	 public void navigateTonavigateToInsertionTryEditor() {
+	       clickOninsertion();
+	       clickTryHere();
+	    }
+	 public void navigateTonavigateToDeletionTryEditor() {
+	        clickOndeletionLink();
+	        clickTryHere();
+	    }
+	 
+	 
+	    
+	    
+	 
+	 
+
+}
---
+++ src/test/java/testCases/HomeTest.java
@@ -0,0 +1,213 @@
+package testCases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import jdk.internal.org.jline.utils.Log;
+import pageFactory.BasePage;
+import pageFactory.Home_pf;
+import pageFactory.Launch_pf;
+import utils.LoggerLoad;
+//@Listeners({ CustomListener.class })
+public class HomeTest extends BaseTest  {
+public HomeTest() throws IOException {
+		super();
+		
+	}
+Launch_pf Launchpf;
+Home_pf Homepf;
+BasePage base;
+LoggerLoad log;
+	
+	@BeforeMethod
+	@Parameters("browser")
+	public void setup() throws IOException, InterruptedException {
+			
+	 Launchpf = new Launch_pf();
+	 Homepf =new Home_pf();
+	 this.log = new LoggerLoad();
+	 Launchpf.get_testUrl();
+	 Launchpf.clickgetstarted();
+	}
+		 
+			@Test(priority=1)
+		    void ClicksDataStBtn()
+		    {
+				Homepf.clickgetstartedDatastructures();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("check DS getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+	
+		 @Test(priority=2)
+		    void ClickArrayBtn()
+		    {
+			 Homepf.clickgetstartedArray();
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("click on Array getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+		
+		 @Test(priority=3)
+		    void ClickLinkedlstBtn() 
+		    {
+			 
+			 Homepf.clickgetstartedLinkedList();
+			
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("click on LinkedLst getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+		
+		 @Test(priority=4)
+		    void ClickStacktBtn() 
+		    {
+			 
+			 Homepf.clickgetstartedStack();
+			
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("click on Stack getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+		
+		
+		
+		
+
+		 @Test(priority=5)
+		    void ClickQueueBtn() throws InterruptedException
+		    {
+			 
+			 Homepf.clickgetstartedQueue();
+			
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("click on Queue getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+	
+		 @Test(priority=6)
+		    void ClickTreeBtn() 
+		    {
+			 
+			 Homepf.clickgetstartedTree();
+			
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("click on Tree getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+	
+		 @Test(priority=7)
+		    void ClickGraphBtn() 
+		    {
+			 
+			 Homepf.clickgetstartedGraph();
+			
+				String actmsg = Homepf.notloggedinmesg();
+				String expmsg = "You are not logged in";
+				log.info("click on Graph getstarted");
+				Assert.assertEquals(actmsg, expmsg,
+						"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		    }
+	
+		@Test(priority=8)
+		void DropdownDS()
+		{
+			Homepf.clickDropdownbox();
+			allureScreenshot();
+			log.error("the user is not able to see mesg your are not logged in because no option in the dropdown");
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			log.info("click on Dropdown DS ");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=9)
+		void DropdownArray()
+		{
+			Homepf.clickDropdownbox();
+			Homepf.clickDropdownArrays();
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			Log.info("click on Dropdown Array ");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=10)
+		void DropdownLinkedlst()
+		{
+			Homepf.clickDropdownbox();
+			Homepf.clickDropdownLinkedList();
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			Log.info("click on Dropdown Linkedlst");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=11)
+		void DropdownStack()
+		{
+			Homepf.clickDropdownbox();
+			Homepf.clickDropdownStack();
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			Log.info("click on Dropdown Stack ");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=12)
+		void DropdownQueue()
+		{
+			Homepf.clickDropdownbox();
+			Homepf.clickDropdownQueue();
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			Log.info("click on Dropdown Queue ");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=13)
+		void DropdownTree()
+		{
+			Homepf.clickDropdownbox();
+			Homepf.clickDropdownTree();
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			Log.info("click on Dropdown Tree ");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=14)
+		void DropdownGraph()
+		{
+			Homepf.clickDropdownbox();
+			Homepf.clickDropdownGraph();
+			String actmsg = Homepf.notloggedinmesg();
+			String expmsg = "You are not logged in";
+			Log.info("click on Dropdown Graph ");
+			Assert.assertEquals(actmsg, expmsg,
+					"the user is not able to see mesg your are not logged in because no option in the dropdown");
+		}
+		@Test(priority=15)
+		 void clickregister() {
+			Homepf.clickRegister();
+		}
+
+		 
+
+}
